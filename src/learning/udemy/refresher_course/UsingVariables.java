48ad10cb3962ce66a044d910e17ca446bac762d8
walter banda <mmurimakai@gmail.com>
2020-05-24 23:24:52 +0300

    My second Commit

---
+++ src/learning/udemy/refresher_course/UsingVariables.java
@@ -0,0 +1,30 @@
+package learning.udemy.refresher_course;
+
+public class UsingVariables {
+    public static void main(String[] args) {
+        //int
+        int myNumber; 
+        /*
+            this is the integer variable it s a storage location that stores integers
+            int are 32bit values
+        */
+        myNumber = 88;
+        System.out.println("this is a integer datatype and the values is "+myNumber);
+        //Short
+        short myShort;
+        myShort = 900;
+        System.out.println("This is a short datatype and its values is "+myShort);
+        //Long datatype
+        long myLong;
+        myLong = 8588;
+        System.out.println("This is the long datatype and its value is "+myLong);
+        //Float
+        float myFloat;
+        myFloat = 184.5f;
+        System.out.println("This is the float datatype and its value is "+myFloat);
+        //Double datatype
+        double myDouble;
+        myDouble = 184.5;
+        System.out.println("This is the float datatype and its value is "+myDouble);
+    }
+}
---
+++ src/learning/udemy/refresher_course/Working_with_text.java
@@ -0,0 +1,5 @@
+package learning.udemy.refresher_course;
+
+public class Working_with_text {
+    
+}
