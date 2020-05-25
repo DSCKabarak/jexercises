ab6fae87661723a5e850e9d2dd349a1629e4266d
walter banda <mmurimakai@gmail.com>
2020-05-25 20:13:47 +0300

    Ive finished with working with text

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("The ide i am refreshing my programming knowledge is vscode");
--- src/learning/udemy/refresher_course/Working_with_text.java
+++ src/learning/udemy/refresher_course/Working_with_text.java
@@ -1,5 +1,13 @@
 package learning.udemy.refresher_course;
 
 public class Working_with_text {
-    
+    public static void main(String[] args) {
+        int myInt = 7;//Integer required for custom output
+        String text = "Hello";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+        System.out.println(greeting);
+        System.out.println("My interger is"+myInt);
+    }
 }
